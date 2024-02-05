72e5c142834a887037bde071813f3ed346cd8817
Leandro Gabriel <leogabrields97@gmail.com>
2024-02-05 14:01:23 -0300

    created LoginRouter

---
+++ lab_clinicas_self_service/lib/src/modules/auth/login/login_router.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+
+import '../../../services/user_login_service.dart';
+import '../../../services/user_login_service_impl.dart';
+
+import 'login_controller.dart';
+import 'login_page.dart';
+
+class LoginRouter extends FlutterGetItModulePageRouter {
+  const LoginRouter({super.key});
+
+  @override
+  List<Bind<Object>> get bindings => [
+        Bind.lazySingleton<UserLoginService>(
+            (i) => UserLoginServiceImpl(userRepository: i())),
+        Bind.lazySingleton((i) => LoginController(loginService: i())),
+      ];
+
+  @override
+  WidgetBuilder get view => (_) => const LoginPage();
+}
